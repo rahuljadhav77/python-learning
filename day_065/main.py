d71e4d4c05564ed38c4dddf8ade1101ffcd05c90
Python Student <python-student@example.com>
2026-03-06 21:20:44 +0530

    Day 65: Explored Exploring Strings: Text Formatter

--- day_065/main.py
+++ day_065/main.py
@@ -1,1 +1,4 @@
-# Day 65: Text Formatter\ndef bold(text): return f'**{text}**'\ndef italic(text): return f'*{text}*'\nprint(bold(italic('Hello World')))\n
+# Day 65: Text Formatter
+def bold(text): return f'**{text}**'
+def italic(text): return f'*{text}*'
+print(bold(italic('Hello World')))
